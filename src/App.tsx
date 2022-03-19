532ece166f05141afe58e2c8fbf564e5b20c4627
caddoweb <douglas.caddoweb@gmail.com>
2022-03-19 17:32:41 -0300

    Iniciando o projeto de visualização em 3d

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { Suspense } from 'react';
+
+import { Canvas } from '@react-three/fiber'
+import Model from './components/models-3d/Model-draco'
+import NewItem from './components/models-3d/Newitem'
+import Iphone from './components/models-3d/Iphone'
 import './App.css';
+import { OrbitControls } from '@react-three/drei';
 
 function App() {
+  
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Canvas>
+        <OrbitControls enableZoom={true}/>
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Model/>
+        </Suspense>
+      </Canvas>
+      <Canvas className='canvas3'>
+        <OrbitControls enableZoom={true}/>
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <Iphone/>
+        </Suspense>
+      </Canvas>
+      <Canvas className='canvas2'>
+        <OrbitControls enableZoom={true}/>
+        <ambientLight intensity={0.5}/>
+        <directionalLight position={[-2, 5, 2]} intensity={1} />
+        <Suspense fallback={null}>
+          <NewItem/>
+        </Suspense>
+      </Canvas>
     </div>
   );
 }
